1a46833b5e729692ffa6722b8f6cdff8bce7e987
Sushant J <sushant.or.j@gmail.com>
2017-05-20 18:25:48 +0530

    Added homeview with list Created navbar Added global scss files sass-loader added to project for the same

---
+++ src/components/_NavBarComponent/_NavBarComponent.vue
@@ -0,0 +1,15 @@
+<template lang="html" src="./navbar.html"></template>
+
+<script>
+export default {
+  name: 'navbar',
+  data () {
+    return {
+      createdMsg: 'Created the navbar'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped src="./navbar.scss">
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HomeComponent from '@/components/HomeComponent/HomeComponent'
+import _NavBarComponent from '@/components/_NavBarComponent/_NavBarComponent'
 
 Vue.use(Router)
+Vue.component('navbar', _NavBarComponent)
 
 export default new Router({
   routes: [
